69db11d8551e3718919a64141b9e5115c5fe9190
Ming Zhang <ming.zhang02@sap.com>
2019-11-12 19:57:08 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## Introduction
+
+This project is inspired by the JAM official project https://github.com/SAP/SAPJamSampleCode/tree/master/JamOAuth2AccessTokenfromSAMLClientCLI. 
+That is a JAVA project which illustrates authentication of the SAP Jam Collaboration API using an OAuth2 access token obtained from a SAML2 bearer assertion.
+
+However, we want to have a nodejs project since we want to have the whole **OAuth2SAMLBearerAssertion** flow implemeneted in nodejs BFF.
+node-saml is most important dependency.
+
+## Usage
+
+1. Copy your JAM signing private key into resource folder, overwrite the file private.key.
+2. Copy your JAM X509 Certificate into resource folder, overwrite the file cert.pem.
+3. Run this project with following arguments provided:
+- baseUrl: The base url of the Jam site
+- clientKey: Jam OAuth client key
+- idpId: Identifier for the SAML trusted IDP
+- subjectNameIdFormat: "email" or "unspecified"
+- subjectNameId: The identifier for the user. Can be an email address or a unique identifier, depending on the subjectNameIdFormat
---
+++ index.js
@@ -0,0 +1,68 @@
+const saml20 = require('saml').Saml20;
+const fs = require('fs');
+const querystring = require('querystring');
+const yargs = require('yargs');
+const axios = require('axios');
+
+const argv = yargs
+    .option('baseUrl', {
+        description: 'The base url of the Jam site',
+        type: 'string',
+    })
+    .option('clientKey', {
+        description: 'Jam OAuth client key',
+        type: 'string',
+    })
+    .option('idpId', {
+        description: 'Identifier for the SAML trusted IDP',
+        type: 'string',
+    })
+    .option('subjectNameIdFormat', {
+        description: '"email" or "unspecified"',
+        type: 'string',
+    })
+    .option('subjectNameId', {
+        description: 'The identifier for the user. Can be an email address or a unique identifier, depending on the subjectNameIdFormat',
+        type: 'string',
+    })
+    .demandOption(['baseUrl', 'clientKey', 'idpId', 'subjectNameIdFormat', 'subjectNameId'], 'Please provide all arguments to work with this tool')
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+const options = {
+  cert: fs.readFileSync(__dirname + '/resource/cert.pem'),
+  key: fs.readFileSync(__dirname + '/resource/private.key'),
+  issuer: argv.idpId,
+  lifetimeInSeconds: 600,
+  audiences: 'cubetree.com', //as obtained from SAP Jam docs 
+  recipient: argv.baseUrl + '/api/v1/auth/token',
+  attributes: {
+    'client_id': argv.clientKey,
+  },
+  nameIdentifier: argv.subjectNameId,
+  nameIdentifierFormat: argv.subjectNameIdFormat === 'email'? 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress': 'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified',
+  signatureAlgorithm: 'rsa-sha1',
+  digestAlgorithm: 'sha1',
+};
+
+// create the saml assertion based on given input, and sign it with the idp private key
+const signedAssertion = '<?xml version="1.0" encoding="UTF-8"?>\n' + saml20.create(options);
+console.log("saml assertion generated: " + signedAssertion);
+const signedAsserationBase64 = Buffer.from(signedAssertion).toString('base64');
+
+// saml bearer assertion -> OAuth saml bearer token
+const config = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+};
+axios.post(argv.baseUrl + '/api/v1/auth/token', querystring.stringify({ 
+    client_id: argv.clientKey,
+    grant_type: 'urn:ietf:params:oauth:grant-type:saml2-bearer',
+    assertion: signedAsserationBase64
+}), config).then( (response) => {
+    console.log("token generated: " +  response.data.access_token);
+  }).catch((err) => {
+    console.error(err);
+  });
